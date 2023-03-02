201f0bedf281a27c9b1431bdf9002a0f4c907eb5
Nicholas Garcia <nicholasgarciaaa@gmail.com>
2023-03-01 21:41:29 -0700

    header and notes menu toggle

--- src/App.js
+++ src/App.js
@@ -1,5 +1,48 @@
+import { useState } from "react";
+
 function App() {
-  return <h1>Lotion</h1>;
+  function toggleNotes(){
+    let x = document.getElementById("notes");
+    let y = document.getElementById("editor");
+    if(x.style.display === "none") {
+      x.style.display = "block";
+      y.setAttribute("style", "width: 85%");
+    }
+    else {
+      x.style.display = "none";
+      y.setAttribute("style"," width: 100%");
+    }
+  }
+
+  return (
+    <div>
+      <div class="header-flex">
+        <label id="menu" onClick={toggleNotes}>&#9776;</label>
+        <div id="title">
+          <h1>Lotion</h1>
+          <p>Like Notion, but worse.</p>
+        </div>
+      </div>
+
+      <div class="page">
+        <div id="notes">
+          <div id="notes-header">
+            <h1>Notes</h1>
+            <label><h2>+</h2></label>
+          </div>
+          <div id="notes-menu">
+            
+          </div>
+
+        </div>
+
+        <div id="editor">
+          <h1>Select A Note, Or Create A New One</h1>
+        </div>
+      </div>
+
+    </div>
+  );
 }
 
 export default App;
